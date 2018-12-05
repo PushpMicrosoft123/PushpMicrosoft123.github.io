3007e7e1d148515869af6a41c8c949e97dde9a67
Pushpdeep Gupta <45087848+PushpMicrosoft123@users.noreply.github.com>
2018-12-05 16:11:38 +0530

    Add files via upload

---
+++ compare-tool5.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    inputOldHtml = $('#old-data').html();
+    inputNewHtml = $('#new-data').html();
+    targetSelector = '#result';
+    this.tool = null;
+    var _ = this;
+    $('#compare').on('click', function (e) {
+        _.tool = new NUI.CompareTool({
+            oldHtml: inputOldHtml,
+            newHtml: inputNewHtml,
+            target: targetSelector
+        });
+        $(targetSelector).empty();
+        console.log(_.tool.compare());
+        $(targetSelector).append(_.tool.compare());
+    });
+    $('#clear').on('click', function (e) {
+        _.tool.clear();
+    });
+});
